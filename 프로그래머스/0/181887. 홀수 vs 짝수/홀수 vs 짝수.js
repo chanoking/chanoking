928f6508a87918c58e779e0fbd5dd69d30959652
chano <134757772+chanoking@users.noreply.github.com>
2024-08-02 13:09:18 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,7 +1,16 @@
+// const solution = (num_list) => {
+//     const evenSum = num_list.filter((_,i)=> i%2!==0).reduce((acc, curr) => acc + curr, 0);
+//     const oddSum = num_list.filter((_,i) => i%2===0).reduce((acc, curr) => acc + curr, 0);
+    
+    
+//     return evenSum === oddSum ? evenSum : evenSum > oddSum ? evenSum : oddSum;
+// }
+
 const solution = (num_list) => {
-    const evenSum = num_list.filter((_,i)=> i%2!==0).reduce((acc, curr) => acc + curr, 0);
-    const oddSum = num_list.filter((_,i) => i%2===0).reduce((acc, curr) => acc + curr, 0);
+    let even = 0;
+    let odd = 0;
     
+    num_list.map((v, i) => !(i%2) ? even += v : odd += v)
     
-    return evenSum === oddSum ? evenSum : evenSum > oddSum ? evenSum : oddSum;
+    return even > odd ? even : odd;
 }
