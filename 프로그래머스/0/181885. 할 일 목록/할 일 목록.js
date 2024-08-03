8add1f823a85849ef6bc40786a8f406944ec4d8b
chano <134757772+chanoking@users.noreply.github.com>
2024-08-03 10:43:51 +0900

    [level 0] Title: 할 일 목록, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,13 @@
+// const solution = (todo_list, finished) => {
+//     const arr = {};
+//     todo_list.forEach((a)=> arr[a] = finished.forEach((b) => b) )
+//     return arr;
+// }    
+
+const solution = (todo_list, finished) => {
+    const arr = {};
+    
+    todo_list.forEach((a) => arr[a] = (...finished.forEach((a) => a)))
+    
+    return arr;
+}
