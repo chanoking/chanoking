868df95d460f7030da979f2f9ce8795c7c040a77
chano <134757772+chanoking@users.noreply.github.com>
2024-08-10 15:21:40 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.97 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -0,0 +1,25 @@
+// const solution = (arr) => {
+//     const result = [];
+//     arr.forEach((e) => )
+//     while(return arr.forEach((e) => e.length) > 0){
+//         result.push(e)
+//     }
+//     return result;
+// // }
+
+// const solution = (arr) => {
+//     const newArr = arr.map((e) => arr.fill(e))
+//     return [...newArr]
+// }
+
+const solution = (arr) => {
+    const answer = [];
+    answer.length = 4
+    answer.fill(4)
+    return answer;
+    // for(let i = 0; i < arr.reduce((acc, curr) => acc + curr); i++){
+    //     // return [].fill(arr[i])
+    //     // answer.push(...[].fill(arr[i]))
+    // }
+    // return answer;
+}
