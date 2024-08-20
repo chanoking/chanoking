757c4ccc77b56f11b8eea306711dbb2dcd8f7ef2
chano <134757772+chanoking@users.noreply.github.com>
2024-08-20 12:41:12 +0900

    [level 1] Title: K번째수, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,5 +1,14 @@
+// const solution = (array, commands) => {
+//     const arr = [];
+//     commands.forEach((e,i) => arr.push(array.slice(e[0] - 1,e[1]).sort((a,b) => a -b)[e[2] - 1]))
+//     return arr;
+// }
+
 const solution = (array, commands) => {
-    const arr = [];
-    commands.forEach((e,i) => arr.push(array.slice(e[0] - 1,e[1]).sort((a,b) => a -b)[e[2] - 1]))
-    return arr;
+    return commands.map(command => {
+        const [iPosition, jPosition, kPosition] = command;
+        const newArray = array.filter((_,i) => i >= iPosition-1 && i <= jPosition -1).sort((a,b) => a - b);
+        
+        return newArray[kPosition - 1]
+    })
 }
