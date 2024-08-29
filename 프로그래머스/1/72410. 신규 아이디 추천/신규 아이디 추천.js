3d22063a902fb2af3dc1494c69a8e553262bc367
chano <134757772+chanoking@users.noreply.github.com>
2024-08-29 12:20:48 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.30 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -31,28 +31,299 @@
 //     return new_id;
 // }
 
+// const solution = (new_id) => {
+//     new_id = new_id.toLowerCase();
+    
+//     new_id = new_id.replace(/[^a-z0-9-._]/g,'');
+    
+//     new_id = new_id.replace(/\.{2,}/g,".")
+    
+//     new_id = new_id.replace(/^\.|\.$/g,'')
+    
+//     if(new_id === "") {
+//         new_id = 'a'
+//     }
+    
+//     if(new_id.length >= 16){
+//         new_id = new_id.slice(0,15)
+//     }
+    
+//     new_id = new_id.replace(/\.$/, '')
+    
+//     while(new_id.length <= 2){
+//         new_id += new_id.charAt(new_id.length - 1)
+//     }
+    
+//     return new_id;
+// }
+
+// const solution = (new_id) => {
+//     new_id = new_id.toLowerCase();
+    
+//     new_id = new_id.replace(/[^a-z0-9-._]/g,"");
+    
+//     new_id = new_id.replace(/\.{2,}/g,".");
+    
+//     new_id = new_id.replace(/^\.|\.$/g,"");
+    
+//     if(new_id === ""){
+//         new_id = "a";
+//     }
+    
+//     if(new_id.length >= 16){
+//         new_id = new_id.slice(0,15);
+//     }
+    
+//     new_id = new_id.replace(/\.$/,"");
+    
+//     while(new_id.length <= 2){
+//         new_id += new_id.charAt(new_id.length - 1)
+//     }
+    
+//     return new_id;
+// }
+// const solution = (new_id) => {
+//     new_id = new_id.toLowerCase('');
+    
+//     new_id = new_id.replace(/[^a-z0-9-._]/g, "");
+    
+//     new_id = new_id.replace(/\.{2,}/g, ".");
+    
+//     new_id = new_id.replace(/^\.|\.$/g, "");
+    
+//     if(new_id === ""){
+//         new_id = "a";
+//     }
+    
+//     if(new_id.length >= 16){
+//         new_id = new_id.slice(0,15);
+//     }
+    
+//     new_id = new_id.replace(/\.$/,"");
+    
+//     while(new_id.length<=2){
+//         new_id += new_id.charAt(new_id.length - 1);
+//     }
+    
+//     return new_id;
+// }
+
+
+
+
+
+
+
+
+// function solution(new_id) {
+//     const answer = new_id
+//         .toLowerCase() // 1
+//         .replace(/[^\w-_.]/g, '') // 2
+//         .replace(/\.+/g, '.') // 3
+//         .replace(/^\.|\.$/g, '') // 4
+//         .replace(/^$/, 'a') // 5
+//         .slice(0, 15).replace(/\.$/, ''); // 6
+//     const len = answer.length;
+//     return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
+// }
+
+
+
+// const solution = (new_id) => {
+    
+//     const answer = new_id
+//     .toLowerCase()
+//     .replace(/[^\w-._]/g,"")
+//     .replace(/\.+/g, '.')
+//     .replace(/^\.|\.$/g,"")
+//     .replace(/^$/,'a')
+//     .slice(0,15).replace(/\.$/,"")
+    
+//     const len = answer.length
+//     return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3-len)
+// }
+
+// const solution = (new_id) => {
+//     const answer = new_id
+//     .toLowerCase()
+//     .replace(/[^\w-_.]/g, "")
+//     .replace(/\.+/g, ".")
+//     .replace(/^\.|\.$/g, "")
+//     .replace(/^$/, "a")
+//     .slice(0,15)
+//     .replace(/\.$/,"")
+    
+//     const len = answer.length;
+    
+//     return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3-len);
+// }
+
+// const solution = (new_id) => {
+//     const answer = new_id
+//     .toLowerCase()
+//     .replace(/[^\w-._]/g,"")
+//     .replace(/\.+/g,".")
+//     .replace(/^\.|\.$/g,"")
+//     .replace(/^$/,"a")
+//     .slice(0, 15).replace(/\.$/,"")
+    
+//     const len = answer.length;
+    
+//     return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3-len);
+// }
+
+
 const solution = (new_id) => {
-    new_id = new_id.toLowerCase();
+    const answer = new_id
+    .toLowerCase()
+    .replace(/[^\w-_.]/g, "")
+    .replace(/\.+/g,".")
+    .replace(/^\.|\.$/g,"")
+    .replace(/^$/,"a")
+    .slice(0,15).replace(/\.$/,"")
     
-    new_id = new_id.replace(/[^a-z0-9-._]/g,'');
+    const len = answer.length;
     
-    new_id = new_id.replace(/\.{2,}/g,".")
-    
-    new_id = new_id.replace(/^\.|\.$/g,'')
-    
-    if(new_id === "") {
-        new_id = 'a'
-    }
-    
-    if(new_id.length >= 16){
-        new_id = new_id.slice(0,15)
-    }
-    
-    new_id = new_id.replace(/\.$/, '')
-    
-    while(new_id.length <= 2){
-        new_id += new_id.charAt(new_id.length - 1)
-    }
-    
-    return new_id;
+    return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3-len);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
