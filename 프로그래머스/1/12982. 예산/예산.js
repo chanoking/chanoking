1a4d2fda685e29f02890a1aff5c8e76e864511c8
chano <134757772+chanoking@users.noreply.github.com>
2024-09-04 13:01:03 +0900

    [level 1] Title: 예산, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,68 @@
+// const solution = (d, budget) => {
+//     let result = 0;
+//     let sortedArr = d.sort((a,b) => a - b);
+//     while(budget>-Infinity){
+//         sortedArr.forEach((v,i) => {
+//            if(budget >= v){
+//                 result++;   
+//            }
+//            budget -= v;
+//        } 
+//     )}
+    
+//     return result;
+// }
+
+// const solution = (d, budget) => {
+//     const sortedD = d.sort((a,b) => a-b);
+//     let result = 0;
+    
+//     for(let i = 0; i < sortedD.length; i++){
+//         if(sortedD[i] <= budget){
+//             result++;
+//             budget -= sortedD[i];
+//         }else{
+//             break;
+//         }
+//     }
+    
+//     return result;
+// }
+
+const solution = (d, budget) => {
+    const sortedD = d.sort((a,b) => a - b);
+    let result = 0;
+    
+    for(let i = 0; i < sortedD.length; i++){
+        if(budget >= sortedD[i]){
+            result++;
+            budget -= sortedD[i];
+        }
+    }
+    
+    return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
