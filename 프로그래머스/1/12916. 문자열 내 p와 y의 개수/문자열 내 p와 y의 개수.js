f147dd1cced1462268a526073ce5fc04afba9c99
chano <134757772+chanoking@users.noreply.github.com>
2024-09-04 12:34:25 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,14 +1,19 @@
+// const solution = (s) => {
+//     s = s.toLowerCase();
+//     const map = {}
+//     let pCount = 0;
+//     let yCount = 0;
+//     for(let e of s){
+//         if(e === 'p'){
+//             pCount++;
+//         }else if(e === 'y'){
+//             yCount++;
+//         }
+//     }
+//     return pCount === yCount ? true : false;
+// }
+
 const solution = (s) => {
-    s = s.toLowerCase();
-    const map = {}
-    let pCount = 0;
-    let yCount = 0;
-    for(let e of s){
-        if(e === 'p'){
-            pCount++;
-        }else if(e === 'y'){
-            yCount++;
-        }
-    }
-    return pCount === yCount ? true : false;
+
+    return s.toUpperCase().split('Y').length === s.toUpperCase().split('P').length ? true : false;
 }
