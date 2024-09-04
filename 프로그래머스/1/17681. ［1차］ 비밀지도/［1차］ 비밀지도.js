1c3730c15020e66d322a0e4e549667c9d6e2a7dc
chano <134757772+chanoking@users.noreply.github.com>
2024-09-04 12:24:03 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.34 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -59,36 +59,143 @@
 //     return answer;
 // }
 
+// const solution = (n, arr1, arr2) => {
+//     // 이진수로 변환하고 길이를 n으로 맞추는 함수
+//     const toBinaryString = (num) => {
+//         let binary = num.toString(2); // 이진수 문자열로 변환
+//         return binary.padStart(n, '0'); // 길이를 n으로 맞추기 위해 앞에 0을 추가
+//     };
+
+//     // 두 배열의 각 원소를 이진수 문자열로 변환
+//     let binaryArr1 = arr1.map(toBinaryString);
+//     let binaryArr2 = arr2.map(toBinaryString);
+
+//     // 이진수 문자열을 '#'과 ' '으로 변환
+//     let mapOne = binaryArr1.map(binary => binary.replace(/1/g, '#').replace(/0/g, ' '));
+//     let mapTwo = binaryArr2.map(binary => binary.replace(/1/g, '#').replace(/0/g, ' '));
+
+//     // 두 배열을 비교하여 결과를 생성
+//     let answer = [];
+//     for (let i = 0; i < n; i++) {
+//         let char = '';
+//         for (let j = 0; j < n; j++) {
+//             // 두 배열의 해당 위치가 모두 '#'이면 '#'을 추가
+//             if (mapOne[i][j] === '#' || mapTwo[i][j] === '#') {
+//                 char += '#';
+//             } else {
+//                 // 둘 다 공백이면 공백 추가
+//                 char += ' ';
+//             }
+//         }
+//         answer.push(char);
+//     }
+
+//     return answer;
+// };
+
+
+
+// const solution = (n, arr1, arr2) => {
+//     const toBinaryString = (num) => {
+//         const binary = num.toString(2);
+//         return binary.padStart(n,0);
+//     }
+    
+//     let arr1Binary = arr1.map((v) => toBinaryString(v));
+//     let arr2Binary = arr2.map((v) => toBinaryString(v));
+    
+//     let mapOne = arr1Binary.map((v) => v.replace(/0/g,' ').replace(/1/g, "#"))
+//     let mapTwo = arr2Binary.map((v) => v.replace(/0/g,' ').replace(/1/g, "#"))
+    
+//     let answer = [];
+    
+//     for(let i = 0; i < n; i++){
+//         let char = '';
+//         for(let j = 0; j < n; j++){
+//             if(mapOne[i][j] === '#' || mapTwo[i][j] === '#'){
+//                 char += '#';
+//             }else{
+//                 char += ' '
+//             }
+//         }
+//         answer.push(char)
+//     }
+    
+//     return answer;
+// }
+
+
+// function solution(n, arr1, arr2) {
+//     return arr1.map((v, i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? '#' : ' '));
+// }
+
+// const addZero = (n, s) => {
+//     return '0'.repeat(n - s.length) + s;
+// }
+
+
+
+
+
+// const solution = (n, arr1, arr2) => {
+    
+//     return arr1.map((v,i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? '#' : ' '));
+// }
+    
+//     const addZero = (n,s) => {
+//         return '0'.repeat(n - s.length) + s
+//     }
+
+
+
+
+
+
+
+
 const solution = (n, arr1, arr2) => {
-    // 이진수로 변환하고 길이를 n으로 맞추는 함수
-    const toBinaryString = (num) => {
-        let binary = num.toString(2); // 이진수 문자열로 변환
-        return binary.padStart(n, '0'); // 길이를 n으로 맞추기 위해 앞에 0을 추가
-    };
+    return arr1.map((v,i) => addZero(n,(v|arr2[i]).toString(2)).replace(/0|1/g, a => +a ? '#' : ' '));
+}
 
-    // 두 배열의 각 원소를 이진수 문자열로 변환
-    let binaryArr1 = arr1.map(toBinaryString);
-    let binaryArr2 = arr2.map(toBinaryString);
+const addZero = (n, s) => {
+    return '0'.repeat(n - s.length) + s;
+}
 
-    // 이진수 문자열을 '#'과 ' '으로 변환
-    let mapOne = binaryArr1.map(binary => binary.replace(/1/g, '#').replace(/0/g, ' '));
-    let mapTwo = binaryArr2.map(binary => binary.replace(/1/g, '#').replace(/0/g, ' '));
 
-    // 두 배열을 비교하여 결과를 생성
-    let answer = [];
-    for (let i = 0; i < n; i++) {
-        let char = '';
-        for (let j = 0; j < n; j++) {
-            // 두 배열의 해당 위치가 모두 '#'이면 '#'을 추가
-            if (mapOne[i][j] === '#' || mapTwo[i][j] === '#') {
-                char += '#';
-            } else {
-                // 둘 다 공백이면 공백 추가
-                char += ' ';
-            }
-        }
-        answer.push(char);
-    }
 
-    return answer;
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
