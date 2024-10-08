e4f225cbc2da9ab9b2c67ec38a42b5d58d3b7474
chano <134757772+chanoking@users.noreply.github.com>
2024-10-08 11:41:05 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 2156.69 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,30 +1,45 @@
-const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+// const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
 
-const solution = (arrayA, arrayB) => {
-    const gcdA = arrayA.reduce((acc, cur) => gcd(acc, cur));
-    const gcdB = arrayB.reduce((acc, cur) => gcd(acc, cur));
-    const result = [];
-//     for(let i = 2; i <= gcdA; i++){
-//         if(gcdA % i === 0 && arrayB.every((b) => b % i !== 0)){
-//             result.push(i);
+// const solution = (arrayA, arrayB) => {
+//     const gcdA = arrayA.reduce((acc, cur) => gcd(acc, cur));
+//     const gcdB = arrayB.reduce((acc, cur) => gcd(acc, cur));
+//     const result = [];
+// //     for(let i = 2; i <= gcdA; i++){
+// //         if(gcdA % i === 0 && arrayB.every((b) => b % i !== 0)){
+// //             result.push(i);
+// //         }
+// //     }
+    
+// //     for(let i = 2; i <= gcdB; i++){
+// //         if(gcdB % i === 0 && arrayA.every((a) => a % i !== 0)){
+// //             result.push(i)
+// //         }
+// //     }
+    
+//      if(arrayB.every((b) => b % gcdA !== 0)){
+//             result.push(gcdA);
 //         }
-//     }
     
-//     for(let i = 2; i <= gcdB; i++){
-//         if(gcdB % i === 0 && arrayA.every((a) => a % i !== 0)){
-//             result.push(i)
+    
+//      if(arrayA.every((a) => a % gcdB !== 0)){
+//             result.push(gcdB);
 //         }
-//     }
     
-     if(arrayB.every((b) => b % gcdA !== 0)){
-            result.push(gcdA);
-        }
+//     return result.length === 0 ? 0 : Math.max(...result);
     
-    
-     if(arrayA.every((a) => a % gcdB !== 0)){
-            result.push(gcdB);
-        }
-    
-    return result.length === 0 ? 0 : Math.max(...result);
-    
+// }
+
+function solution(arrayA, arrayB) {
+    const aResult = getAnswer(arrayA, arrayB)
+    const bResult = getAnswer(arrayB, arrayA)
+
+    return aResult > bResult ? aResult : bResult
+}
+
+function getAnswer (A, B) {
+    A.sort((a, b) => a - b)
+    for (let i = A[0]; i > 1; i--) {
+        if (A.every(a => a % i === 0) && !B.some(b => b % i === 0)) return i
+    }
+    return 0
 }
