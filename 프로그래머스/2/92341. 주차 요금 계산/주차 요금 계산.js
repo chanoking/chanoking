5b939d9035a9e823d878e673fea1134786444b6f
chano <134757772+chanoking@users.noreply.github.com>
2024-10-17 12:50:44 +0900

    [level 2] Title: 주차 요금 계산, Time: 4.20 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,29 +1,53 @@
+// const solution = (fees, records) => {
+//     const calculation = (standardPrice, accumulated, standard, unit, unitPrice) => {
+//         return standard >= accumulated ? standardPrice : standardPrice + Math.ceil((accumulated - standard) / unit) * unitPrice;
+//     }
+//     const [standard, standardPrice, unit, unitPrice] = fees;
+//     const condition = 23 * 60 + 59;
+//     const tracking = {};
+//     const sub = {};
+//     const result = [];
+//     const another = new Set();
+//     records.forEach((record) => {
+//         const [time, num, io] = record.split(' ');
+//         const [hh, mm] = time.split(":").map(a => Number(a));
+//         const intoM = hh * 60 + mm;
+//         if(!sub[num]) sub[num] = {"IN" : 0, "OUT" : 0};
+//         sub[num][io]++;
+//         if(io === "IN") tracking[num] = (tracking[num] || 0) - intoM;
+//         else tracking[num] += intoM
+//         another.add(num);
+//     })
+//     const another2 = [...another].sort();
+//     another2.forEach((num) => {
+//         if(sub[num]["IN"] !== sub[num]["OUT"]) tracking[num] += condition;
+//         const fee = calculation(standardPrice, tracking[num], standard, unit, unitPrice);
+//         result.push(fee);
+//     })
+    
+//     return result;
+// }
+
 const solution = (fees, records) => {
-    const calculation = (standardPrice, accumulated, standard, unit, unitPrice) => {
-        return standard >= accumulated ? standardPrice : standardPrice + Math.ceil((accumulated - standard) / unit) * unitPrice;
-    }
-    const [standard, standardPrice, unit, unitPrice] = fees;
-    const condition = 23 * 60 + 59;
-    const tracking = {};
-    const sub = {};
-    const result = [];
-    const another = new Set();
+    const history = {};
+    const box = [];
     records.forEach((record) => {
-        const [time, num, io] = record.split(' ');
-        const [hh, mm] = time.split(":").map(a => Number(a));
-        const intoM = hh * 60 + mm;
-        if(!sub[num]) sub[num] = {"IN" : 0, "OUT" : 0};
-        sub[num][io]++;
-        if(io === "IN") tracking[num] = (tracking[num] || 0) - intoM;
-        else tracking[num] += intoM
-        another.add(num);
-    })
-    const another2 = [...another].sort();
-    another2.forEach((num) => {
-        if(sub[num]["IN"] !== sub[num]["OUT"]) tracking[num] += condition;
-        const fee = calculation(standardPrice, tracking[num], standard, unit, unitPrice);
-        result.push(fee);
+        const [t, n, w] = record.split(' ');
+        const [h, m] = t.split(':').map(a => Number(a));
+        const intoM = h * 60 + m;
+        if(!history[n]) history[n] = 0;
+        if(w === "IN"){
+            history[n] += (1439 - intoM);
+        }else{
+            history[n] -= (1439 - intoM);
+        }
     })
     
-    return result;
+    for(let [n,t] of Object.entries(history)){
+        if(t <= fees[0]) t = fees[1];
+        else t = fees[1] + Math.ceil((t - fees[0]) / fees[2]) * fees[3];
+        box.push([n, t]);
+    }
+    
+    return box.sort((a, b) => a[0] - b[0]).map(a => a[1])
 }
