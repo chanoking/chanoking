89f2a8cc2e85b0d90b85fb1d61c6c7acff86b959
chano <134757772+chanoking@users.noreply.github.com>
2024-10-28 12:26:05 +0900

    [level 2] Title: n^2 배열 자르기, Time: 8.30 ms, Memory: 49.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,26 @@
+// const solution = (n, left, right) => {
+//     const arr = Array.from({length: n}, () => Array(n).fill(0));
+//     let num = 1
+//     const numb = Array.from({length: n}, (_,i) => i);
+//     let ni = 0;
+//     const result = [];
+//     for(let i = 0; i < n; i++){
+//         for(let j = 0; j < n; j++){
+//             numb.forEach((n) => {
+//                 if(i === n || j === n) arr[i][j] = n + 1;
+//             })
+//         }
+//     }
+//     arr.forEach((a) => a.forEach(b => result.push(b)))
+//     return result.slice(left, right + 1);
+// }
+
+const solution = (n, left, right) => {
+    const result = [];
+    for (let k = left; k <= right; k++) {
+        const i = Math.floor(k / n);
+        const j = k % n;
+        result.push(Math.max(i, j) + 1);
+    }
+    return result;
+};
