7f3202279cfc88335a1d2b0944afba12259a2c28
chano <134757772+chanoking@users.noreply.github.com>
2024-11-13 12:29:22 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,25 +1,25 @@
 const solution = (n, wires) => {
     const g = Array.from({length: n}, () => []);
-    for(let e of wires){
-        g[e[0] - 1].push(e[1] - 1);
-        g[e[1] - 1].push(e[0] - 1);
-    }
-    const q = [0];
     const p = new Array(n).fill(-1);
+    const q = [0];
+    for(let e of wires){
+        g[e[1] - 1].push(e[0] - 1);
+        g[e[0] - 1].push(e[1] - 1);
+    }
     for(let i = 0; i < q.length; i++){
         const u = q[i];
-        for(let v of g[u])if(v !== p[u] ){
+        for(let v of g[u])if(v !== p[u]){
             p[v] = u;
             q.push(v);
         }
     }
     
     let ans = n;
-    const dp = new Array(n).fill(1);
+    const dp = Array(n).fill(1);
     for(let i = q.length; --i > 0;){
         const v = q[i];
         dp[p[v]] += dp[v];
-        const a = Math.abs(n - dp[v] * 2);
+        const a = Math.abs(n - dp[v] * 2)
         if(ans > a) ans = a;
     }
     return ans;
