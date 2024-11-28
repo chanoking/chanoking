5d04e8604854c419bed1f476726c3c12f8cd9aaa
chano <134757772+chanoking@users.noreply.github.com>
2024-11-28 16:54:57 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 21.63 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,11 +1,11 @@
 const solution = (s) => {
+    let str = '';
     let count = 0;
     let count0 = 0;
     while(s !== '1'){
         count0 += s.split('').filter(c => c === '0').length;
-        s = s.replace(/0/g, '').length.toString(2);
+        s = s.replace(/0/g,'').length.toString(2);
         count++;
     }
-    
     return [count, count0];
 }
