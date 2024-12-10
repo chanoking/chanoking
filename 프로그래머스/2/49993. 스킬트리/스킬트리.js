7302f8556249e74c8521f253c3ca78b1a2424bc2
chano <134757772+chanoking@users.noreply.github.com>
2024-12-10 14:05:32 +0900

    [level 2] Title: 스킬트리, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,41 +1,5 @@
-// const solution = (skill, skill_trees) => {
-//     let whether = true;
-//     let count = 0;
-//         for(let sk of skill_trees){
-//             whether = true;
-//             const arr = [];
-//             for(let s of skill) {
-//                 const i = sk.indexOf(s);
-//                 if(arr.length === 0){
-//                     arr.push(i)
-//                 }else{
-//                     if(arr.pop() > i){
-//                         arr.push(i)
-//                         whether = false;
-//                         break;
-//                     }
-//                 }
-//             }
-//             if(whether === true) count++;
-//         }
-//     return count;
-// }
-
 const solution = (skill, skill_trees) => {
-    let count = 0;
-    let switching;
-    for(let skills of skill_trees){
-        const reversed = skill.split('').reverse();
-        switching = true;
-        const reverse = skills.split('').reverse();
-        const deleted = reverse.filter(v => reversed.includes(v));
-        for(let i = 0; i < deleted.length;){
-            if(reversed.pop() !== deleted.pop()){
-                switching = false;
-                break;
-            }
-        }
-        if(switching === true) count++;
-    }
-        return count;
+    const regex = new RegExp(`[^${skill}]`, 'g');
+    return skill_trees.map(v => v.replace(regex, '')).filter(
+    v => skill.indexOf(v) === 0 || v === '').length;
 }
