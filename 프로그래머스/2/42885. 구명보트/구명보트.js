ed2f63aba1066ea9480da178f9cf4a1cad82870b
chano <134757772+chanoking@users.noreply.github.com>
2024-12-12 12:19:47 +0900

    [level 2] Title: 구명보트, Time: 35.89 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,31 @@
+// const solution = (people, limit) => {
+//     let count = 0;
+//     let adjusted = people.sort((a, b) => a - b);
+//     for(let i = adjusted.length; i > 0;){
+//         const bundle = adjusted.slice(0, i);
+//         const sum = bundle.reduce((acc, cur) => acc + cur, 0);
+//         if(sum <= limit) {
+//             count++;
+//             i -= bundle.length;
+//         }else{
+//             i--;
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+const solution = (people, limit) => {
+    let count = 0;
+    let left = 0;
+    let right = people.length - 1;
+    const sorted = people.sort((a, b) => a - b);
+    while(left <= right){
+        if((sorted[left] + sorted[right]) <= limit){
+            left++;
+        }
+        right--;
+        count++;
+    }
+    return count;
+}
