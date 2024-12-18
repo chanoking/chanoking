9fdd1a2a26f95d41aef3af4a3527966cb24eedc7
chano <134757772+chanoking@users.noreply.github.com>
2024-12-18 11:43:53 +0900

    [level 2] Title: 더 맵게, Time: 522.11 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -1,51 +1,62 @@
-// const solution = (scoville, k) => {
-//      const verify = (arr) => {
-//         return arr.every(v => v >= k);
+// class MiniHeap {
+//     constructor(){
+//         this.heap = [];
 //     }
-//     const minHeap = scoville.sort((a, b) => a - b); // 최소 힙처럼 사용
-
-//     let count = 0;
-
-//     while (minHeap.length > 1) {
-//         if(verify(minHeap)) return count;
-//         // 가장 작은 두 개의 요소 꺼내기
-//         const first = minHeap.shift();
-//         const second = minHeap.shift();
-
-//         // 스코빌 지수 계산
-//         const newScoville = first + second * 2;
-
-//         // 계산한 값을 힙에 추가하고 정렬 유지
-//         minHeap.unshift(newScoville);
-//         // minHeap.sort((a, b) => a - b); // 힙 정렬
-//         count++;
-
-//         // 모든 값이 K 이상인지 확인
-//         if (minHeap[0] >= k) return count;
+//     heapifyUp(){
+//         let index = this.heap.length - 1;
+//         while(index > 0){
+//             let parentIndex = Math.floor((index - 1)/ 2);
+//             if(this.heap[index] >= this.heap[parentIndex]) break;
+//             [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+//             index = parentIndex;
+//         }
 //     }
-
-//     // 모든 경우를 섞어도 K를 달성하지 못한 경우
-//     return -1;
-// };
+//     heapifyDown(){
+//         let index = 0;
+//         let length = this.heap.length;
+//         while(true){
+//             let leftChild = index * 2 + 1;
+//             let rightChild = index * 2 + 2;
+//             let smallest = index;
+//             if(this.heap[smallest] > this.heap[leftChild] && length > leftChild) smallest = leftChild;
+//             if(this.heap[smallest] > this.heap[rightChild] && length > rightChild) smallest = rightChild;
+//             if(smallest === index) break;
+//             [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+//             index = smallest;
+//         }
+//     }
+//     pop(){
+//         if(this.heap.length === 1) return this.heap[0];
+//         const root = this.heap[0];
+//         this.heap[0] = this.heap.pop();
+//         this.heapifyDown();
+//         return root;
+//     }
+//     push(value){
+//         this.heap.push(value);
+//         this.heapifyUp();
+//     }
+//     size(){
+//         return this.heap.length;
+//     }
+//     peak(){
+//         return this.heap[0];
+//     }
+// }
 
 // const solution = (scoville, K) => {
-//     const heap = [];
 //     let count = 0;
-//     const verify = (arr) => {
-//         return arr.every(v => v >= K);
-//     }
-//     if(verify(scoville)) return count;
-//     while(scoville.length > 0){
-//         const firstMini = Math.min(...scoville);
-//         scoville.splice(scoville.indexOf(firstMini), 1)
-//         const secondMini = Math.min(...scoville);
-//         scoville.splice(scoville.indexOf(secondMini), 1)
-//         const newValue = firstMini + (secondMini * 2);
-//         scoville.push(newValue);
+//     const miniHeap = new MiniHeap();
+//     scoville.forEach(v => miniHeap.push(v));
+//     while(miniHeap.size() > 1){
+//         let first = miniHeap.pop();
+//         if(first >= K) return count;
+//         let second = miniHeap.pop();
+//         let newScoville = first + second * 2;
+//         miniHeap.push(newScoville);
 //         count++;
-//         if(verify(scoville)) return count;
 //     }
-//     return -1;
+//     return miniHeap.peak() >= K ? count : -1;
 // }
 
 class MinHeap {
@@ -109,10 +120,13 @@ class MinHeap {
 
 const solution = (scoville, K) => {
     const minHeap = new MinHeap();
+    
     scoville.forEach((num) => minHeap.push(num));
 
     let count = 0;
+    
     while (minHeap.size() > 1) {
+        // console.log(minHeap);
         const first = minHeap.pop();
         if (first >= K) return count;
 
@@ -121,7 +135,6 @@ const solution = (scoville, K) => {
         minHeap.push(newScoville);
 
         count++;
-    }
-
+    }    
     return minHeap.peek() >= K ? count : -1;
 };
