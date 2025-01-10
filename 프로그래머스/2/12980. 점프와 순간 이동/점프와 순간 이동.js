433770b7931cf7dfb4742add69be4270c86fb996
chano <134757772+chanoking@users.noreply.github.com>
2025-01-10 10:28:41 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,14 +1,5 @@
 const solution = (N) => {
-    let power = 0;
-
-    while (N > 0) {
-        if (N % 2 === 0) {  // If N is even, divide by 2 (no power used)
-            N /= 2;
-        } else {             // If N is odd, jump (subtract 1 and use 1 power)
-            N -= 1;
-            power++;
-        }
-    }
-
-    return power;
+    if(N === 1) return 1;
+    const NArr = Array.from(N.toString(2));
+    return NArr.reduce((a, b) => +a + +b);
 }
