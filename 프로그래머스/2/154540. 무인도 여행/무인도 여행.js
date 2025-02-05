c861276a4d9871cced3267a4198ed92e81e7b81d
chano <134757772+chanoking@users.noreply.github.com>
2025-02-05 17:28:22 +0900

    [level 2] Title: 무인도 여행, Time: 10.39 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -1,36 +1,32 @@
 const solution = (maps) => {
-    const dx = [-1,1,0,0]
-    const dy = [0,0,-1,1]
-    const arr = maps.map(v => v.split(''));
-    const cols = maps[0].length, rows = maps.length;
-    const visited = Array.from({length: rows}, () => Array(cols).fill(false));
-    const answer = [];
+    const rows = maps.length, cols = maps[0].length;
+    const visited = Array.from({length : rows}, () => Array(cols).fill(false));
+    const dx = [1, -1, 0, 0];
+    const dy = [0, 0, 1, -1];
     const isValid = (x, y) => {
-        if(x >= 0 && y >= 0 && x < rows && y < cols){
-            return true;
-        }
-        return false;
+        return x >= 0 && x < rows && y >= 0 && y < cols;
     }
-    const dfs = (r, c) => {
-        let count = parseInt(arr[r][c]);
-        visited[r][c] = true;
-        for(let k = 0; k < 4; k++){
-            let nx = r + dx[k];
-            let ny = c + dy[k];
-            if(isValid(nx, ny) && !visited[nx][ny] && arr[nx][ny] !== 'X'){
+    const arr = maps.map(v => v.split(''));
+    const dfs = (x, y) => {
+        let count = parseInt(arr[x][y])
+        visited[x][y] = true;
+        for(let k = 0; k < dx.length; k++){
+            const nx = x + dx[k];
+            const ny = y + dy[k];
+            if(isValid(nx,ny) && !visited[nx][ny] && arr[nx][ny] !== 'X'){
                 count += dfs(nx, ny);
             }
         }
         return count;
     }
-    
+    const answer = [];
     for(let i = 0; i < rows; i++){
         for(let j = 0; j < cols; j++){
             if(!visited[i][j] && arr[i][j] !== 'X'){
-                let sum = dfs(i, j);
-                answer.push(sum);
+                let totalDays = dfs(i, j);
+                answer.push(totalDays)
             }
         }
     }
-    return answer.length > 0 ? answer.sort((a,b) => a - b) : [-1]
+    return answer.length > 0 ? answer.sort((a,b) => a - b) : [-1];
 }
