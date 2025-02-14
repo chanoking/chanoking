0c5cd868f58c165d17e1149487c4246d99b4d5e6
chano <134757772+chanoking@users.noreply.github.com>
2025-02-14 11:01:29 +0900

    [level 2] Title: 테이블 해시 함수, Time: 43.85 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,17 +1,11 @@
-function solution(data, col, row_begin, row_end) {
-  data.sort((a, b) => {
-      if(a[col - 1] === b[col - 1]){
-          return b[0] - a[0];
-      }else{
-          return a[col - 1] - b[col - 1];   
-      }
-  })
-  const sums = Array(row_end - row_begin + 1).fill(0)
-  for(let i = row_begin - 1; i <= row_end - 1; i++){
-      for(let j = 0; j < data[i].length; j++){
-          sums[i - row_begin + 1] += data[i][j] % (i + 1);
-      }
-  }
-  
-  return sums.reduce((acc, cur) => acc ^ cur, 0);
+const sort = (a, b, col) => {
+  return a[col - 1] - b[col - 1] || b[0] - a[0]
+}
+
+const solution = (data, col, row_begin, row_end) => {
+  data.sort((a, b) => sort(a, b, col));
+
+  return data.map((row, i) => row.reduce((acc, c) => acc + (c % (i + 1)),0))
+  .slice(row_begin-1, row_end)
+  .reduce((acc, cur) => acc ^ cur, 0);
 }
