687833aa66614ff69c4261b54dbbf06762a8c4de
chano <134757772+chanoking@users.noreply.github.com>
2025-02-24 11:39:51 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
@@ -26,22 +26,107 @@
 // }
 
 
-function solution(players, m, k) {
-    let servers = Array(24).fill(0);
+// function solution(players, m, k) {
+//     let servers = Array(24).fill(0);
+//     let answer = 0;
+
+//     players.forEach((playerCnt, sTime) => {
+//         if (parseInt(playerCnt / m) > servers[sTime]) {
+//             let needServerCnt = parseInt(playerCnt / m) - servers[sTime];
+
+//             for (let i = 0; i < k; i++) {
+//                 if (sTime + i <= 23) {
+//                     servers[sTime + i] = servers[sTime + i] + needServerCnt;
+//                 }
+//             }
+//             answer += needServerCnt;
+//         }
+//     });
+//     return answer;
+// }
+
+// const solution = (players, m, k) => {
+//     let answer = 0;
+//     const timeline = Array(24).fill(0);
+//     players.forEach((p, pi) => {
+//         const needed = parseInt(p / m);
+//         if(needed > timeline[pi]){
+//             timeline[pi] = needed - timeline[pi];
+        
+//         for(let i = 0; i < k; i++){
+//             if(pi + i <= 23){
+//                 timeline[pi + i] += needed + timeline[pi + i];
+//                 }
+//             }
+//         }
+//         answer += needed;
+//     })
+//     return answer;
+// }
+
+const solution = (players, m, k) => {
+    const servers = Array(24).fill(0);
     let answer = 0;
-
-    players.forEach((playerCnt, sTime) => {
-        if (parseInt(playerCnt / m) > servers[sTime]) {
-            let needServerCnt = parseInt(playerCnt / m) - servers[sTime];
-
-            for (let i = 0; i < k; ++i) {
-                if (sTime + i <= 23) {
-                    servers[sTime + i] = servers[sTime + i] + needServerCnt;
+    players.forEach((p, pi) => {
+        if(parseInt(p / m) > servers[pi]){
+            let needed = parseInt(p / m) - servers[pi];
+            
+            for(let i = 0; i < k; ++i){
+                if(i + pi <= 23){
+                    servers[pi + i] = servers[pi + i] + needed;
                 }
             }
-            answer += needServerCnt;
+            answer += needed;
         }
-    });
-
+    })
+    
     return answer;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
