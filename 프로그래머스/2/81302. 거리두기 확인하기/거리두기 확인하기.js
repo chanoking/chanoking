a7bdfe0ba7bb30d4d7ce20a1a920c2ce5c267b4b
chano <134757772+chanoking@users.noreply.github.com>
2025-03-06 12:28:33 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.41 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
@@ -1,43 +1,36 @@
 const solution = (places) => {
-
-    const checkDistancing = (place) => {
+    const checkDistances = (place) => {
         const people = [];
-
-        // Collect all 'P' positions
         for(let i = 0; i < 5; i++){
             for(let j = 0; j < 5; j++){
-                if(place[i][j] === "P"){
+                if(place[i][j] === 'P'){
                     people.push([i, j]);
                 }
             }
         }
-
+        
         for(let i = 0; i < people.length; i++){
             for(let j = i + 1; j < people.length; j++){
                 const [x1, y1] = people[i];
                 const [x2, y2] = people[j];
-
+                
                 const distance = Math.abs(x1 - x2) + Math.abs(y1 - y2);
-
-                if (distance > 2) continue;
-
-                if(distance === 1){
+                
+                if(distance > 2) continue;
+                if(distance === 1) {
                     return 0;
                 }else if(distance === 2){
-                    if(x1 === x2) {
-                        if(place[x1][(y1 + y2) / 2]  !== 'X') return 0;
-                    } else if(y1 === y2){
+                    if(x1 === x2){
+                        if(place[x1][(y2 + y1)/ 2] !== 'X') return 0;
+                    }else if(y1 === y2){
                         if(place[(x1 + x2) / 2][y1] !== 'X') return 0;
-                    } else{
-                        if(!(place[x1][y2] === 'X' && place[x2][y1] === 'X')){
-                            return 0;
-                        }
+                    }else{
+                        if(!(place[x1][y2] === 'X' && place[x2][y1] === 'X')) return 0;
                     }
                 }
             }
         }
         return 1;
     }
-
-    return places.map(checkDistancing);
+    return places.map(checkDistances);
 }
