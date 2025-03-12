6df0d5afcbe2367bdaf6b00c3a422970eba65730
chano <134757772+chanoking@users.noreply.github.com>
2025-03-12 12:09:05 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 24.16 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
@@ -1,9 +1,9 @@
 const solution = (row, column, queries) => {
-    const matrix = [...Array(row)].map((_, r) => [...Array(column)].map((_, c) => r * column + c + 1));
+    const matrix = [...Array(row)].map((_, r) => [...Array(column)].map((_, c) => r * column + 1 + c));
     const answer = [];
     
     queries.forEach(query => {
-        const [x1, y1, x2, y2] = query.map(_ => _  - 1);
+        let [x1, y1, x2, y2] = query.map(_ => _ - 1);
         let min = matrix[x1][y1], temp = matrix[x1][y1];
         
         for(let i = x1; i < x2; i++){
@@ -23,11 +23,10 @@ const solution = (row, column, queries) => {
         
         for(let i = y2; i > y1; i--){
             matrix[x1][i] = matrix[x1][i - 1];
-            min = Math.min(min, matrix[x1][i - 1]);
+            min = Math.min(min, matrix[x1][i])
         }
-        
-        matrix[x1][y1 + 1] = temp;
-        answer.push(min);
+        matrix[x1][y1+1] = temp;
+        answer.push(min)
     })
     
     return answer;
