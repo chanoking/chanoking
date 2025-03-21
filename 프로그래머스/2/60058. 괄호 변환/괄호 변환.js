04218c0c6b2185c3b745b5b25ca00cfff08a8253
chano <134757772+chanoking@users.noreply.github.com>
2025-03-21 12:21:22 +0900

    [level 2] Title: 괄호 변환, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
@@ -1,32 +1,33 @@
 const solution = (w) => {
     if(w === '') return w;
     let u, v, cnt = 0;
-    
     for(let i = 0; i < w.length; i++){
-        w[i] === ')' ? cnt++ : cnt--;
-        if(cnt === 0){
+        w[i] === ")" ? cnt++ : cnt--;
+        if(cnt === 0) {
             u = w.slice(0, i+1);
-            v = w.slice(i+1);
+            v = w.slice(i+1);  
             break;
-        }
+        } 
     }
-    
+
     if(!isPerfect(u)){
         let str = '';
         str += `(${solution(v)})`;
-        for(let i = 1; i < u.length - 1; i++){
-            u[i] === ")" ? str += "(" : str += ")";
+        for(let i = 1; i < u.length-1; i++){
+            u[i] === ")" ? str += "(" : str += ")"; 
         }
         return str;
     }
+    
     return u + solution(v);
+
 }
+
 const isPerfect = (str) => {
-        return (str[0] === ")" || str[str.length - 1] === "(") ? false : true;
-        let cnt = 0;
-        for(let i = 0; i < str.length; i ++){
-            str[i] === ")" ? cnt++ : cnt--;
-        }
-        
-        return cnt === 0;
+    return (str[0] === "(" && str[str.length - 1] === ")");
+    let cnt = 0;
+    for(let i = 0; i < str.length; i++){
+        str[i] === ")" ? cnt++ : cnt--;
     }
+    return cnt === 0;
+}
