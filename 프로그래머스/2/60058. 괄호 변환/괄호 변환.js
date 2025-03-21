b597c330c878012a187b5ae048520cf0700da894
chano <134757772+chanoking@users.noreply.github.com>
2025-03-21 12:31:37 +0900

    [level 2] Title: 괄호 변환, Time: 0.29 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
@@ -1,33 +1,7 @@
-const solution = (w) => {
-    if(w === '') return w;
-    let u, v, cnt = 0;
-    for(let i = 0; i < w.length; i++){
-        w[i] === ")" ? cnt++ : cnt--;
-        if(cnt === 0) {
-            u = w.slice(0, i+1);
-            v = w.slice(i+1);  
-            break;
-        } 
-    }
-
-    if(!isPerfect(u)){
-        let str = '';
-        str += `(${solution(v)})`;
-        for(let i = 1; i < u.length-1; i++){
-            u[i] === ")" ? str += "(" : str += ")"; 
-        }
-        return str;
-    }
-    
-    return u + solution(v);
-
+const reverse = (str) => {
+    return str.slice(1, str.length - 1).split('').map(v => v === ")" ? v = "(" : v = ")").join('');
 }
 
-const isPerfect = (str) => {
-    return (str[0] === "(" && str[str.length - 1] === ")");
-    let cnt = 0;
-    for(let i = 0; i < str.length; i++){
-        str[i] === ")" ? cnt++ : cnt--;
-    }
-    return cnt === 0;
+const solution = (w) => {
+    return reverse(w);
 }
