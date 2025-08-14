e385abe92fa386a387afccd5fac8ebbbbc044f47
chano <134757772+chanoking@users.noreply.github.com>
2025-08-14 11:34:25 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,14 +1,20 @@
-SELECT e1.ID
-FROM ECOLI_DATA AS e1
-INNER JOIN (
-    SELECT e2.ID
-    FROM ECOLI_DATA AS e2
-    INNER JOIN (
-        SELECT e3.ID
-        FROM ECOLI_DATA AS e3
-        WHERE e3.PARENT_ID IS NULL
-    ) AS A
-        ON A.ID = e2.PARENT_ID
-) AS C
-    ON e1.PARENT_ID = C.ID
-ORDER BY e1.ID;
+WITH RECURSIVE hierarchy AS (
+    -- 1단계: 루트 노드 찾기
+    SELECT ID, PARENT_ID, 0 AS depth
+    FROM ECOLI_DATA
+    WHERE PARENT_ID IS NULL
+
+    UNION ALL
+
+    -- 이후 단계: 부모 ID를 가진 자식 찾기
+    SELECT e.ID, e.PARENT_ID, h.depth + 1
+    FROM ECOLI_DATA AS e
+    INNER JOIN hierarchy AS h
+        ON e.PARENT_ID = h.ID
+        -- 여기서 깊이 제한 가능 (당신 쿼리는 2단계까지만)
+        WHERE h.depth < 2
+)
+SELECT ID
+FROM hierarchy
+WHERE DEPTH = 2
+ORDER BY ID;
