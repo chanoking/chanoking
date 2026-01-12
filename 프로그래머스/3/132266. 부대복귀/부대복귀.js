21769d933b6990aa2fe0538ad341ee3898093962
chano <134757772+chanoking@users.noreply.github.com>
2026-01-12 16:40:19 +0900

    [level 3] Title: 부대복귀, Time: 865.44 ms, Memory: 181 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.js
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.js
@@ -1,26 +1,27 @@
-const solution = (n, roads, sources, destination) => {
-  const bridges = Array.from({ length: n + 1 }, () => []);
-
-  for (let [roadA, roadB] of roads) {
-    bridges[roadA].push(roadB);
-    bridges[roadB].push(roadA);
+function solution(n, roads, sources, destination) {
+  const graph = new Array(n + 1).fill(null).map((_) => []);
+  for (let [a, b] of roads) {
+    graph[a].push(b);
+    graph[b].push(a);
   }
-
-  const steps = Array(n + 1).fill(-1);
-
-  const queue = [destination];
-
-  let idx = 0;
-  steps[destination] = 0;
-  while (idx < queue.length) {
-    let c = queue[idx++];
-    for (let point of bridges[c]) {
-      if (steps[point] === -1) {
-        steps[point] = steps[c] + 1;
-        queue.push(point);
+  const visited = new Array(n + 1).fill(Infinity);
+  const bfs = (destination) => {
+    const q = [destination];
+    visited[destination] = 0;
+    while (q.length > 0) {
+      const idx = q.shift();
+      for (let newIdx of graph[idx]) {
+        if (visited[idx] + 1 < visited[newIdx]) {
+          visited[newIdx] = visited[idx] + 1;
+          q.push(newIdx);
+        }
       }
     }
-  }
+  };
+  bfs(destination);
 
-  return sources.map((source) => steps[source]);
-};
+  return sources.map((v) => {
+    if (visited[v] === Infinity) return -1;
+    else return visited[v];
+  });
+}
